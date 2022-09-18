9e4f9283b98c66cbf59458a4bc8f38fbdd96b7ba
Auros Nexus <41306347+Auros@users.noreply.github.com>
2022-09-18 14:42:25 -0400

    GameObject deactivation based on CanvasGroup state

---
+++ Assets/Runtime/UI/Utilities/DeactivateWhenCanvasGroupsAreZero.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+namespace Flux.UI
+{
+    public class DeactivateWhenCanvasGroupsAreZero : MonoBehaviour
+    {
+        [SerializeField]
+        private CanvasGroup[] _canvasGroups = Array.Empty<CanvasGroup>();
+
+        private void Update()
+        {
+            foreach (var canvasGroup in _canvasGroups)
+                canvasGroup.gameObject.SetActive(!Mathf.Approximately(canvasGroup.alpha, 0));
+        }
+    }
+}
